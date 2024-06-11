e466970b176542bf94ed499f2ca4288da70b72c9
Sanmesh Kakade <sanmeshskakade@gmail.com>
2024-06-11 19:20:06 +0530

    3.1-limit-service-time-per-user

--- 3.1-limit-service-time-per-process/main.go
+++ 3.1-limit-service-time-per-process/main.go
@@ -10,6 +10,10 @@
 
 package main
 
+import (
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +25,24 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+
+	done := make(chan bool)
+
+	go func() {
+		process()
+		done <- true
+	}()
+
+	for {
+		select {
+		case <-done:
+			return true
+		case <-time.After(time.Second * 10):
+			if !u.IsPremium {
+				return false
+			}
+		}
+	}
 }
 
 func main() {
